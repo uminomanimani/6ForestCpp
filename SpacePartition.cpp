01fe3ed75714dc79574ca5cabd5eb6c71db74f4e
止めどなく <ma_likai@icloud.com>
2023-04-28 11:34:28 +0800

    modified

--- SpacePartition.cpp
+++ SpacePartition.cpp
@@ -1,7 +1,23 @@
 #include "include/SpacePartition.h"
 #include <queue>
 
-std::vector<std::vector<std::vector<char>>> SpacePartition(std::vector<std::vector<char>> arrs)
+std::vector<std::vector<std::vector<char>>> SpacePartition(const std::vector<std::vector<char>>& arrs)
 {
     std::queue<std::vector<std::vector<char>>> q;
+    q.push(arrs);
+    std::vector<std::vector<std::vector<char>>> regionArrs;
+    while(!q.empty())
+    {
+        auto front = q.front(); q.pop();
+        if(front.size() < 16)
+        {
+            regionArrs.push_back(front);
+            continue;
+        }
+        auto splits = SeedClustering(front);
+        for(auto& split : splits)
+        {
+            
+        }
+    }
 }
--- include/SpacePartition.h
+++ include/SpacePartition.h
@@ -1,4 +1,4 @@
 #include <vector>
 
-std::vector<std::vector<std::vector<char>>> SpacePartition(std::vector<std::vector<char>> arrs);
-std::vector<std::vector<char>> SeedClustering(std::vector<std::vector<char>> arrs);
+std::vector<std::vector<std::vector<char>>> SpacePartition(const std::vector<std::vector<char>>& arrs);
+std::vector<std::vector<int>> SeedClustering(const std::vector<std::vector<char>>& arrs);
