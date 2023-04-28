b41bdfe7674594ffc641331bf3c422e4507215ae
止めどなく <ma_likai@icloud.com>
2023-04-28 13:00:19 +0800

    modified

--- SpacePartition.cpp
+++ SpacePartition.cpp
@@ -1,7 +1,7 @@
 #include "include/SpacePartition.h"
 #include <queue>
 
-std::vector<std::vector<std::vector<char>>> SpacePartition(const std::vector<std::vector<char>>& arrs)
+std::vector<std::vector<std::vector<char>>> SpacePartition(const std::vector<std::vector<char>>& arrs, int beta = 16)
 {
     std::queue<std::vector<std::vector<char>>> q;
     q.push(arrs);
@@ -9,7 +9,7 @@ std::vector<std::vector<std::vector<char>>> SpacePartition(const std::vector<std
     while(!q.empty())
     {
         auto front = q.front(); q.pop();
-        if(front.size() < 16)
+        if(front.size() < beta)
         {
             regionArrs.push_back(front);
             continue;
@@ -17,7 +17,13 @@ std::vector<std::vector<std::vector<char>>> SpacePartition(const std::vector<std
         auto splits = SeedClustering(front);
         for(auto& split : splits)
         {
-            
+            std::vector<std::vector<char>> tmp;
+            for(auto& i : split)
+            {
+                tmp.push_back(front[i]);
+            }
+            regionArrs.push_back(tmp);
         }
     }
+    return regionArrs;
 }
--- include/IPv6Parse.h
+++ include/IPv6Parse.h
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
 #include <string>
 
-std::vector<std::string> Split(const std::string& s, char seperator);
 std::string Parse(const std::string& IPv6String);
