b720afac77b433e944f8a6af77cd8b9043cce78c
止めどなく <ma_likai@icloud.com>
2023-05-10 13:21:35 +0800

    added #pragma once in headers

--- include/IPv6Parse.h
+++ include/IPv6Parse.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 
 std::string Parse(const std::string& IPv6String);
--- utils.cpp
+++ utils.cpp
@@ -1,15 +1,15 @@
 #include "include/utils.h"
 
-std::vector<std::string> Split(const std::string& s, char seperator)
+std::vector<std::string> Split(const std::string &s, char seperator)
 {
     std::vector<std::string> output;
     std::string::size_type prev_pos = 0, pos = 0;
-    while((pos = s.find(seperator, pos)) != std::string::npos)
+    while ((pos = s.find(seperator, pos)) != std::string::npos)
     {
-        std::string substring(s.substr(prev_pos, pos-prev_pos));
+        std::string substring(s.substr(prev_pos, pos - prev_pos));
         output.push_back(substring);
         prev_pos = ++pos;
     }
-    output.push_back(s.substr(prev_pos, pos-prev_pos));
+    output.push_back(s.substr(prev_pos, pos - prev_pos));
     return output;
 }
