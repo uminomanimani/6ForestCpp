2cad6cfc1efded0a282a44c38a1b2e5476efe5fe
海のまにまに <42465598+uminomanimani@users.noreply.github.com>
2023-05-18 14:53:52 +0800

    Update OutlierSeedDetection.cpp

--- OutlierSeedDetection.cpp
+++ OutlierSeedDetection.cpp
@@ -1,7 +1,7 @@
 #include "include/OutlierSeedDetection.h"
 #include "include/utils.h"
 #include <unordered_set>
-#include <iostream>
+// #include <iostream>
 
 std::pair<std::vector<std::vector<int>>, std::vector<std::vector<int>>> OutlierSeedDetection(const std::vector<std::vector<int>> &arrs, float threshold)
 {
@@ -58,33 +58,3 @@ std::vector<float> IsolationTree(const std::vector<std::vector<int>> &arrs, int 
 
     return weight;
 }
-
-// void testDetection()
-// {
-//     std::vector<std::string> addresses = {
-//         "2a0107c8aacb024c505400fffe2d61ad",
-//         "2a0107c8aacb02540000000000000001",
-//         "2a0107c8aacb02580000000000000001",
-//         "2a0107c8aacb0258505400fffec5f4af",
-//         "2a0107c8aacb02650000000000000001",
-//         "2a0107c8aacb0276505400fffe93673b",
-//         "2a0107c8aacb027a0000000000000000",
-//         "2a0107c8aacb027c0000000000000001",
-//         "2a0107c8aacb027e0000000000000001",
-//         "2a0107c8aacb027f10de51e8eb667583",
-//         "2a0107c8aacb02810000000000000001",
-//         "2a0107c8aacb02900000000000000001",
-//         "2a0107c8aacb02ba505400fffee8eb27",
-//         "2a0107c8aacb02c4505400fffe8312eb"
-//     };
-
-//     std::vector<std::vector<int>> arrs;
-//     for(auto& line : addresses)
-//     {
-//         std::vector<int> arr;
-//         for(auto& i : line)
-//             arr.push_back(hexCharToInt(i));
-//         arrs.push_back(arr);
-//     }
-//     auto r = OutlierSeedDetection(arrs, 5.0f);
-// }
