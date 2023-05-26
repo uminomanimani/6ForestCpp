bedc5d9a1239e3079f1fdcb369bce954cebaeb70
止めどなく <ma_likai@icloud.com>
2023-05-26 11:56:30 +0800

    added run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+g++ -std=c++11 -o main *.cpp
+if [ $? -ne 0 ]; then
+    echo "Compilation failed"
+else
+    ./main -t 2.5 -b 16
+    if [ $? -eq 0 ]; then
+        python ./evaluate.py
+    fi
+fi
